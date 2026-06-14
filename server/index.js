9f083b04ff6e46c56b320028324b1cd2fd70e928
Kushal S <kushal17062004@gmail.com>
2026-06-14 15:09:31 +0530

    Fix CORS origin for production

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors({
     origin:["http://localhost:5173",
-        "https://audify-app.vercel.app",
+        "https://audify-mern-ec5ss1pzx-kushal-ss-projects.vercel.app",
     ],
     methods:["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
