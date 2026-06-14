4501b38b16bb6d2ef767e3a905d7541ae92227ea
Kushal S <kushal17062004@gmail.com>
2026-06-14 19:24:48 +0530

    changed express to get json

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(cors({
     methods:["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
 }));
-app.use(express.json());
+
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
